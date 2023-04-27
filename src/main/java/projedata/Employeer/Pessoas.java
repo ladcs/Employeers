ccf57677f58d916f45e8b0268bc232117d96980c
hplad <hplad@host.docker.internal>
2023-04-27 19:24:14 -0300

    feat: methods pessoas and test to birthDateDayMonthYear

--- src/main/java/projedata/Employeer/Pessoas.java
+++ src/main/java/projedata/Employeer/Pessoas.java
@@ -1,5 +1,34 @@
 package projedata.Employeer;
 
-public class Pessoas {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
+/**
+ * class Pessoa, base to class Funcionario. 
+ */
+public class Pessoas {
+	
+	private String name;
+	private LocalDate birthDate;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+	
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+	
+	public String birthDateDayMonthYear() {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	    return this.birthDate.format(formatter);
+	}
 }
--- src/test/java/projedata/Employeer/PessoasTest.java
+++ src/test/java/projedata/Employeer/PessoasTest.java
@@ -2,6 +2,8 @@ package projedata.Employeer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,17 +12,38 @@ import org.junit.jupiter.api.Test;
  */
 public class PessoasTest {
 	@Test
-	  @DisplayName("Testa se cria a lista corretamente")
-	  void insertAll() {
-		Pessoas p1 = new Pessoas("Garus", "22/09/1988");
-		Pessoas p2 = new Pessoas("Rafael", "03/05/1994");
-		Pessoas p3 = new Pessoas("Luciano", "19/02/1992");
-		
-		assertEquals(p1.getName(), "Garus");
-		assertEquals(p1.getBirthDay(), "22/09/1988");
-		assertEquals(p2.getName(), "Rafael");
-		assertEquals(p2.getBirthDay(), "03/05/1994");
-		assertEquals(p3.getName(), "Luciano");
-		assertEquals(p3.getBirthDay(), "19/02/1992");
-	  }
+	@DisplayName("Testa se cria pessoa e consegue atribuir valores com setter do atributo.")
+	void settersTest() {
+      Pessoas p1 = new Pessoas();
+      p1.setName("Garus");
+      p1.setBirthDate(LocalDate.of(1988, 9, 20));
+      Pessoas p2 = new Pessoas();
+      p2.setName("Rafael");
+      p2.setBirthDate(LocalDate.of(1994, 3, 5));
+      Pessoas p3 = new Pessoas();
+      p3.setName("Luciano");
+      p3.setBirthDate(LocalDate.of(1992, 2, 19));
+      
+      assertEquals(p1.getName(), "Garus");
+      assertEquals(p1.getBirthDate(), LocalDate.of(1988, 9, 20));
+      assertEquals(p2.getName(), "Rafael");
+      assertEquals(p2.getBirthDate(), LocalDate.of(1994, 3, 5));
+      assertEquals(p3.getName(), "Luciano");
+      assertEquals(p3.getBirthDate(), LocalDate.of(1992, 2, 19));
+    }
+	
+	@Test
+	@DisplayName("Testa se o método birthDateDayMonthYear retorna a data como dd/MM/YY")
+	void birthDateTest() {
+		Pessoas p1 = new Pessoas();
+	      p1.setBirthDate(LocalDate.of(1988, 9, 20));
+	      Pessoas p2 = new Pessoas();
+	      p2.setBirthDate(LocalDate.of(1994, 5, 3));
+	      Pessoas p3 = new Pessoas();
+	      p3.setBirthDate(LocalDate.of(1992, 2, 19));
+	      
+	      assertEquals(p1.birthDateDayMonthYear(), "20/09/1988");
+	      assertEquals(p2.birthDateDayMonthYear(), "03/05/1994");
+	      assertEquals(p3.birthDateDayMonthYear(), "19/02/1992");
+	}
 }
