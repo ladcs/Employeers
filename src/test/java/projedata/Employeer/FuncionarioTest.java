92e46f8d5125377a2d31afecaa8156290e919ade
hplad <hplad@host.docker.internal>
2023-04-27 20:11:27 -0300

    feat: test setters class Funcionario

--- src/test/java/projedata/Employeer/FuncionarioTest.java
+++ src/test/java/projedata/Employeer/FuncionarioTest.java
@@ -2,8 +2,11 @@ package projedata.Employeer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import java.math.BigDecimal;
 
 /**
  * Unit test for simple Funcionario.
@@ -11,8 +14,38 @@ import org.junit.jupiter.api.Test;
 public class FuncionarioTest 
 {
 	@Test
-	  @DisplayName("Testa se cria a lista corretamente")
-	  void insertAll() {
-		assertEquals(true, true);
+	  @DisplayName("Testa se cria pessoa e consegue atribuir valores com setter do atributo.")
+	  void settersTests() {
+	   BigDecimal e1Remuneration = new BigDecimal(1000.00);
+	   BigDecimal e2Remuneration = new BigDecimal(1500.00);
+	   BigDecimal e3Remuneration = new BigDecimal(300.00);
+       Funcionario e1 = new Funcionario();
+       e1.setName("Garus");
+       e1.setBirthDate(LocalDate.of(1988, 9, 20));
+       e1.setRemuneration(e1Remuneration);
+       e1.setJob("mecanico");
+       Funcionario e2 = new Funcionario();
+       e2.setName("Rafael");
+       e2.setBirthDate(LocalDate.of(1994, 3, 5));
+       e2.setRemuneration(e2Remuneration);
+       e2.setJob("programador");
+       Funcionario e3 = new Funcionario();
+       e3.setName("Luciano");
+       e3.setBirthDate(LocalDate.of(1992, 2, 19));
+       e3.setRemunaration(e3Remuneration);
+       e3.setJob("auxiliar");
+	      
+       assertEquals(e1.getName(), "Garus");
+       assertEquals(e1.getBirthDate(), LocalDate.of(1988, 9, 20));
+       assertEquals(e1.getRemuneration(), e1Remuneration);
+       assertEquals(e1.getJob(), "mecanico");
+       assertEquals(e2.getName(), "Rafael");
+       assertEquals(e2.getBirthDate(), LocalDate.of(1994, 3, 5));
+       assertEquals(e1.getRemuneration(), e2Remuneration);
+       assertEquals(e1.getJob(), "programador");
+       assertEquals(e3.getName(), "Luciano");
+       assertEquals(e3.getBirthDate(), LocalDate.of(1992, 2, 19));
+       assertEquals(e1.getRemuneration(), e3Remuneration);
+       assertEquals(e1.getJob(), "auxiliar");
 	  }
 }
