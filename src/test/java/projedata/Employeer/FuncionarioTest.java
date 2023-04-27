a52caa5910614b375006f67780eecaeafd135ce5
hplad <hplad@host.docker.internal>
2023-04-27 19:25:09 -0300

    feat: test Funcionario

---
+++ src/test/java/projedata/Employeer/FuncionarioTest.java
@@ -0,0 +1,18 @@
+package projedata.Employeer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple Funcionario.
+ */
+public class FuncionarioTest 
+{
+	@Test
+	  @DisplayName("Testa se cria a lista corretamente")
+	  void insertAll() {
+		assertEquals(true, true);
+	  }
+}
