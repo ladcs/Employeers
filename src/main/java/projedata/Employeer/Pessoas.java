8a80291b09460e2622437431e309c5e32c5bba40
hplad <hplad@host.docker.internal>
2023-04-27 18:21:55 -0300

    :tada:

---
+++ src/main/java/projedata/Employeer/Pessoas.java
@@ -0,0 +1,5 @@
+package projedata.Employeer;
+
+public class Pessoas {
+
+}
---
+++ src/test/java/projedata/Employeer/AppTest.java
@@ -0,0 +1,18 @@
+package projedata.Employeer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	@Test
+	  @DisplayName("Testa se cria a lista corretamente")
+	  void insertAll() {
+		assertEquals(true, true);
+	  }
+}
